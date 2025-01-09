0851284dac836b35ee570c62104bd3db1f75602f
Hassan Ammar <h.ammar@hotmail.de>
2025-01-09 19:10:07 +0100

    rendered and styled additional json info

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -16,12 +16,23 @@ function render() {
   let desserts = document.getElementById('desserts');
   for (let i = 0; i < dataset.length; i++) {
     let dessert = dataset[i];
+    let fortmattedPrice = formatPrice(dessert.price);
     desserts.innerHTML += /*html*/`
       <div class="dessert">
+        <img src= ${dessert.image.desktop} alt= ${dessert.name} class="image">
+        <div class="add-to-cart">
+          <button><img src="./assets/images/icon-add-to-cart.svg" alt="shopping cart with a plus in it">Add to Cart</button>
+        </div>
+        <span class="category">${dessert.category}</span>
         <h4>${dessert.name}</h4>
+        <span class="price">${fortmattedPrice}</span>
       </div>
     `;
   }
+}
+
+function formatPrice(price) {
+  return `$${price.toFixed(2)}`;
 }
 
 
