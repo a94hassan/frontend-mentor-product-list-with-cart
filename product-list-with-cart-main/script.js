0bb1e41a237203d336536b0e3e4f3e34a75bcc2a
Hassan Ammar <h.ammar@hotmail.de>
2025-01-12 02:41:16 +0100

    addToCart(), renderCart(), cart item style

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,10 +1,12 @@
 let dataset;
+let cart = [];
 
 async function init() {
   try {
     const response = await fetch('./data.json');
     dataset = await response.json();
     render();
+    renderCart();
   } catch (error) {
     console.error(error);
   }
@@ -14,18 +16,19 @@ async function init() {
 function render() {
   console.log(dataset);
   let desserts = document.getElementById('desserts');
+  desserts.innerHTML = '';
   for (let i = 0; i < dataset.length; i++) {
     let dessert = dataset[i];
-    let fortmattedPrice = formatPrice(dessert.price);
+    let formattedPrice = formatPrice(dessert.price);
     desserts.innerHTML += /*html*/`
       <div class="dessert">
         <img src= ${dessert.image.desktop} alt= ${dessert.name} class="image">
         <div class="add-to-cart">
-          <button><img src="./assets/images/icon-add-to-cart.svg" alt="shopping cart with a plus in it">Add to Cart</button>
+          <button onclick="addToCart(${i})"><img src="./assets/images/icon-add-to-cart.svg" alt="shopping cart with a plus in it">Add to Cart</button>
         </div>
         <span class="category">${dessert.category}</span>
-        <h4>${dessert.name}</h4>
-        <span class="price">${fortmattedPrice}</span>
+        <h3>${dessert.name}</h3>
+        <span class="price">${formattedPrice}</span>
       </div>
     `;
   }
@@ -33,6 +36,48 @@ function render() {
 
 function formatPrice(price) {
   return `$${price.toFixed(2)}`;
+}
+
+function addToCart(i) {
+  let itemInCart = cart.find(item => item.name === dataset[i].name);
+  if (itemInCart) {
+    itemInCart.quantity++;
+  } else {
+    cart.push({ ...dataset[i], quantity: 1 });
+  }
+  console.table(cart);
+  renderCart();
+}
+
+function renderCart() {
+  let cartContent = document.getElementById('cart-content');
+  cartContent.innerHTML = '';
+  if (cart.length === 0) {
+    cartContent.innerHTML = /*html*/`
+      <img src="./assets/images/illustration-empty-cart.svg" alt="brown cake from which a piece is cut">
+      <span class="empty-cart-text">Your added items will appear here</span>
+    `;
+  } else {
+    for (let i = 0; i < cart.length; i++) {
+      let item = cart[i];
+      let calculatedPrice = item.quantity * item.price;
+      let formattedPrice = formatPrice(item.price);
+      let formattedCalculatedPrice = formatPrice(calculatedPrice);
+      cartContent.innerHTML += /*html*/`
+        <div class="cart-item">
+          <div>
+            <h4>${item.name}</h4>
+            <div class="price-line">
+              <span>${item.quantity}x</span>
+              <span>@ ${formattedPrice}</span>
+              <span>${formattedCalculatedPrice}</span>
+            </div>
+          </div>
+          <img src="./assets/images/icon-remove-item.svg" alt="light brown x">
+        </div>
+      `;
+    }
+  }
 }
 
 
